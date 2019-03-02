9840799e9ea13ecfbb44f333463d9739d20abef4
HateMyLife <ivan.vasyliv0123@gmail.com>
2019-03-02 12:39:15 +0200

    What did you expect?

---
+++ src/Listing.java
@@ -0,0 +1,16 @@
+
+public class Listing {
+
+	public static void main(String[] args) {
+	String info = "";
+	info+= "Team\n";
+	info+= "Kutsenko Michael\n";
+	info+= "Smal'chenko Natalya\n";
+	info+= "Vasyliv Ivan\n";
+	info+= "Theme\n";
+	info+= "Translation by meaning from any language\n";
+	info+= "Name == Lingua Franca";
+	System.out.println(info);
+	}
+
+}
