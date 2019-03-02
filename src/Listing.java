95938c7c282b27fd8c24a6b8b60c6888f59315d8
HateMyLife <ivan.vasyliv0123@gmail.com>
2019-03-02 13:52:09 +0200

    Update Listing.java

--- src/Listing.java
+++ src/Listing.java
@@ -7,7 +7,7 @@ public class Listing {
 	info+= "Kutsenko Michael\n";
 	info+= "Smal'chenko Natalya\n";
 	info+= "Vasyliv Ivan\n";
-	info+= "Theme\n";
+	info+= "Teme\n";
 	info+= "Translation by meaning from any language\n";
 	info+= "Name == Lingua Franca";
 	System.out.println(info);
